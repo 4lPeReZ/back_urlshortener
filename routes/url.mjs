cfce50195561e0fed2f5ea62bad1dd2377fb01d1
4lPeReZ <alvaropr05@hotmail.com>
2024-07-16 22:42:23 +0200

    chore: Add URL shortening functionality with custom URL support

--- routes/url.mjs
+++ routes/url.mjs
@@ -2,6 +2,8 @@
 
 import express from 'express';
 import Url from '../models/Url.mjs';
+import { nanoid } from 'nanoid';
+import { body, validationResult } from 'express-validator';
 import NodeCache from 'node-cache';
 
 const urlCache = new NodeCache({ stdTTL: 600 });
@@ -9,6 +11,38 @@ const router = express.Router();
 
 // Define tu base URL
 const baseUrl = process.env.BASE_URL;
+
+// Ruta para acortar una URL
+router.post('/shorten',
+  body('originalUrl').isURL().withMessage('Invalid URL'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { originalUrl, customUrl, expiresAt } = req.body;
+    const shortUrl = customUrl || nanoid(7);
+
+    try {
+      const url = new Url({ originalUrl, shortUrl, expiresAt });
+      await url.save();
+      const fullShortUrl = `${baseUrl}/${shortUrl}`;
+      res.status(201).json({
+        originalUrl: url.originalUrl,
+        shortUrl: fullShortUrl,
+        userId: url.userId,
+        expiresAt: url.expiresAt,
+        status: url.status,
+        clicks: url.clicks,
+        createdAt: url.createdAt,
+        _id: url._id
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
 
 // Ruta para redireccionar una URL acortada
 router.get('/:shortUrl', async (req, res) => {
